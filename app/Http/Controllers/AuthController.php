2260b8ae740ef33028157aa45cc7a249d2052774
Za1r33 <kcbarbieC@gmail.com>
2022-02-25 18:15:54 +0800

    Sign up Login test

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Hash;
+
+
+class AuthController extends Controller
+{
+
+    //register
+    public function register(Request $request)
+    {
+        $fields = $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|string|unique:users,email',
+            'password' => 'required|string|confirmed'
+        ]);
+
+        $user = User::create([
+            'name' => $fields['name'],
+            'email' => $fields['email'],
+            'password' => bcrypt($fields['password'])
+        ]);
+
+        $token = $user->createToken('myapptoken')->plainTextToken;
+
+        $response = [
+            'user' => $user,
+            'token' =>  $token
+        ];
+
+        return response($response, 201);
+    }
+
+
+    //login
+    public function login(Request $request)
+    {
+        $fields = $request->validate([
+
+            'email' => 'required|string',
+            'password' => 'required|string'
+        ]);
+
+        //check email
+
+        $user = User::where('email', $fields['email'])->first();
+
+        if (!$user || !Hash::check($fields['password'], $user->password)) {
+            return response([
+                'message' => 'Bad credentials'
+            ], 401);
+        }
+
+
+        $token = $user->createToken('myapptoken')->plainTextToken;
+
+        $response = [
+            'user' => $user,
+            'token' =>  $token
+        ];
+
+        return response($response, 201);
+    }
+
+
+
+    //logout
+    public function logout(Request $request)
+    {
+        auth()->user()->tokens()->delete();
+        // $user = Auth::user();
+
+
+        return [
+            'message' => 'Logged out'
+        ];
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,65 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\AuthController;
+use App\Models\Profile;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+//Route::resource('profile', ProfileController::class);
+
+//Public routes
+Route::post('/register',[AuthController::class,'register']);
+Route::post('/login',[AuthController::class,'login']);
+
+Route::get('/profile',[ProfileController::class,'index']);
+Route::get('/profile/search/{fullname}', [ProfileController::class, 'search']);
+Route::get('/profile/{id}',[ProfileController::class,'show']);
+
+
+//Protected routes
+Route::group(['middleware' => ['auth:sanctum']] , function () {
+  Route::post('/profile',[ProfileController::class, 'store']);
+  Route::put('/profile/{id}',[ProfileController::class, 'update']);
+  Route::delete('profile/{id}',[ProfileController::class, 'destroy']);
+  Route::post('/logout',[AuthController::class, 'logout']);
+ 
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// return response()->json([
+//     'fullname' => 'Monstorm' ,
+//     'email' => 'monstorm@gmail.com',
+//     'address' => 'Dalaguete',
+//     'phoneNumber' => '09123979022',
+//     'password' => "no password",
+//     'userTypeID'=> 002,
+
+// ]);
+
+// return Profile::all();
+
+// Route::post('/profile', function () {
+//     return Profile::create([
+//         'fullname' => 'Monstorm',
+//         'email' => 'Monstorm@gmail.com',
+//         'address' => 'Dalaguete'
+//     ]);
+// });
+
+
